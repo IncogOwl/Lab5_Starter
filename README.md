f14596a9ee453faa8b4da2c168fbaafb89c2ae19
IncogOwl <97695545+IncogOwl@users.noreply.github.com>
2024-05-06 16:35:38 -0700

    Added the tests and completed the Check your understanding

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Lab 5 - Starter
 
 Team Members: Varun Parekh
+
+1. If you are using a singular unit test, No. Since the messaging feature has several different aspects and functions running through it that a singular unit test will not be able to test each individual parts of the feature. There would be a unit test to check if it recieved or sent a message and many more things. This would require multiple unit tests and not a single test
+2. Yes, since this is a constant factor in the application and is a reproducible constant that should be tested and ensured in the application. It's very easy to write a unit test that checks if max message length is maintained or not and it is a singular aspect of a feature that is testes thus yes a unit test would be used to it. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,73 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+//isPhoneNumber
+test('Checks Real phone number is true', () => {
+  expect(isPhoneNumber('(123) 456-7890)')).toBe(true);
+});
+test('Real number but wrong format', () => {
+  expect(isPhoneNumber(1234567890)).toBe(false);
+});
+test('Checks real phone number without parenthesis', () => {
+  expect(isPhoneNumber('123-456-7890)')).toBe(true);
+});
+test('Fails for 9 digit number', () => {
+  expect(isPhoneNumber('(123) 456-789)')).toBe(false);
+});
+
+//isEmail
+test('Is True for regular email', () => {
+  expect(isEmail('io@gmail.com')).toBe(true);
+});
+test('Is False for email that does not have @', () => {
+  expect(isEmail('iogmail.com')).toBe(false);
+});
+test('Is False for email that does not have .', () => {
+  expect(isEmail('io@gmailcom')).toBe(false);
+});
+test('Is True for email with any length of username and email domain', () => {
+  expect(isEmail('a@k.ty')).toBe(true);
+});
+
+//isStrongPassword
+test('Is true for regular pass', () => {
+  expect(isStrongPassword('IncogOwl')).toBe(true);
+});
+test('Is false for a pass of length 2', () => {
+  expect(isStrongPassword('io')).toBe(false);
+});
+test('Is true for pass for length of 4', () => {
+  expect(isStrongPassword('A113')).toBe(true);
+});
+test('Is false for pass with spaces', () => {
+  expect(isStrongPassword(' ')).toBe(false);
+});
+
+//isDate
+test('Is true for regular date', () => {
+  expect(isDate('12/13/2024')).toBe(true);
+});
+test('Is false for wrong format', () => {
+  expect(isDate(12/13/2024)).toBe(false);
+});
+test('Is true for random numbers in correct format', () => {
+  expect(isDate('12/34/5678')).toBe(true);
+});
+test('Is false for no forward slashes', () => {
+  expect(isDate('12132024')).toBe(false);
+});
+
+//isHexColor
+test('Is true for correct hex code', () => {
+  expect(isHexColor('#123')).toBe(true);
+});
+test('Is false for hex code longer than 6 digits', () => {
+  expect(isHexColor(1234567)).toBe(false);
+});
+test('Is true for hex code without #', () => {
+  expect(isHexColor('123')).toBe(true);
+});
+test('Is false for hex code with only #', () => {
+  expect(isHexColor('#')).toBe(false);
+});
